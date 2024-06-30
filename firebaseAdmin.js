ea178836113e4fb59205b82e967f9b4152d03b10
Liz Chung <lizzhongx@gmail.com>
2024-06-30 18:10:48 +0900

    no message

---
+++ firebaseAdmin.js
@@ -0,0 +1,15 @@
+import * as admin from 'firebase-admin';
+
+if (!admin.apps.length) {
+  const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+  });
+}
+
+export default admin;
