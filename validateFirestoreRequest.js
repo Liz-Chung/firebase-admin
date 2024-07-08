de4814e748188bacd6a05ac620b51dfef82c7afb
Liz Chung <lizzhongx@gmail.com>
2024-07-08 15:29:43 +0900

    no message

--- validateFirestoreRequest.js
+++ validateFirestoreRequest.js
@@ -1,18 +1,8 @@
-import admin, { firestoreRequest } from '../firebaseAdmin';
-import cors, { runMiddleware } from '../corsMiddleware';
-
-export default async (req, res) => {
-  await runMiddleware(req, res, cors);
-
-  if (req.method === 'POST') {
-    try {
-      const params = req.body;
-      await firestoreRequest(params);
-      res.status(200).send({ message: 'Request validated and processed' });
-    } catch (error) {
-      res.status(400).send({ error: error.message });
-    }
-  } else {
-    res.status(405).send({ error: 'Method not allowed' });
+export function validateFirestoreRequest(params) {
+  if (!params.gsessionid || !params.SID) {
+    throw new Error('Invalid session parameters');
   }
-};
+  if (!params.database) {
+    throw new Error('Database parameter is required');
+  }
+}
